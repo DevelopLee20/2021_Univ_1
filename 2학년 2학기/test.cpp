867e007ecb01064d45f1cab8667eaf6aa5792bda
LeeInGyu <developlee20@gmail.com>
2021-08-31 11:49:00 +0900

    2021-08-31 2학기 준비

---
+++ 2학년 2학기/test.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main(void){
+    cout << "Hello World" << endl << "Kim ChulSoo";
+    // cout << "Hello World" << "\n" << "Kim ChulSoo";
+    return 0;
+}
